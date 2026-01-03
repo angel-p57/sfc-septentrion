afba7360030c19126339864bfeeeee4798fdd92d
angel-p57 <angel.prime57@gmail.com>
2026-01-03 16:26:06 +0900

    delegate canvas operation to TiledCanvas class

--- utils/lib/img.rb
+++ utils/lib/img.rb
@@ -13,3 +13,45 @@ class ImgManager
   end
 end
 
+class TiledCanvas
+  @@linecolor=ImgManager.mkpixel(0,20,20)
+  @@bgcolor=ImgManager.mkpixel(10,10,10,true)
+  class TileWindow
+    def initialize(canvas,trow,tcol)
+      @canvas=canvas
+      @trow=trow
+      @tcol=tcol
+    end
+    def []=(prow,pcol,px)
+      @canvas.dsize.times{|i|
+        @canvas.dsize.times{|j|
+          @canvas[@trow,@tcol,prow,pcol,i,j]=px
+        }
+      }
+    end
+  end
+  def initialize(tsize,dsize,rownum,colnum)
+    @dsize=dsize
+    @tsize=tsize
+    @rownum=rownum
+    @colnum=colnum
+    @tdotnum=@tsize*@dsize+1
+    @vsize=@tdotnum*rownum+1
+    @hsize=@tdotnum*colnum+1
+    @pxlist=@vsize.times.flat_map{|i|
+      i % @tdotnum == 0 ?
+        [@@linecolor]*@hsize :
+        [@@linecolor,*[@@bgcolor]*(@tdotnum-1)]*@colnum+[@@linecolor]
+    }
+  end
+  attr_reader :dsize
+  def []=(trow,tcol,prow,pcol,i,j,px)
+    @pxlist[(trow*@tdotnum+prow*@dsize+i+1)*@hsize+tcol*@tdotnum+pcol*@dsize+j+1]=px
+  end
+  def makewindow(trow,tcol)
+    TileWindow.new(self,trow,tcol)
+  end
+  def save(filename)
+    ImgManager.save(filename,@pxlist,@hsize,@vsize)
+  end
+end
--- utils/mkspimg.rb
+++ utils/mkspimg.rb
@@ -8,7 +8,6 @@ romfile=ARGV[0]||SepRom::DEFAULT_ROM_PATH
 romobj=SepRom.new(romfile)
 vmgr=VRAMmgr.new(romobj)
 
-bg=mkpixel[10,10,10,true]
 blank=mkpixel[16,16,16]
 pallets=9.times.map{|pid|
   vmgr.getspcolors(pid < 8 ? pid : 0).map{|r|
@@ -16,35 +15,25 @@ pallets=9.times.map{|pid|
   } << blank
 }
 pallets[8][12,2]=[mkpixel[31,31,31],mkpixel[0,0,31]]
-lcol=mkpixel[ 0,20,20]
 btile=[[16]*8]*8
 
 tiles2file=->tiles,file,pid,dotsize=3{
   warn "* invalid tile number #{tiles.size} *" if tiles.size%16!=0
   rown=tiles.size/16
-  csizeh=128*dotsize+17
-  csizev=8*rown*dotsize+rown+1
-  canvas=csizev.times.flat_map{|i|
-    i%(dotsize*8+1)==0 ? [lcol]*csizeh : [lcol,*[bg]*(8*dotsize)]*16+[lcol]
-  }
-  (rown*16).times{|i|
-    t=tiles[i] || btile
-    r0=i/16*(dotsize*8+1)+1
-    c0=i%16*(dotsize*8+1)+1
-    8.times{|j|
-      r=r0+j*dotsize
-      8.times{|k|
-        b=r*csizeh+c0+k*dotsize
-        color=pallets[pid][t[j][k]] or next
-        dotsize.times{|n1|
-          dotsize.times{|n2|
-            canvas[b+csizeh*n1+n2]=color
-          }
+  canvas=TiledCanvas.new(8,dotsize,rown,16)
+  rown.times{|r|
+    16.times{|c|
+      win=canvas.makewindow(r,c)
+      t=tiles[r*16+c] || btile
+      8.times{|i|
+        8.times{|j|
+          color=pallets[pid][t[i][j]] or next
+          win[i,j]=color
         }
       }
     }
   }
-  ImgManager.save(file,canvas,csizeh,csizev)
+  canvas.save(file)
 }
 
 sptiles=vmgr.get4bpptiles8(0)
