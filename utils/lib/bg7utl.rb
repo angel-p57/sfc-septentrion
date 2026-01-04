947fb399409da9a2d8f0ab71d1fe8aefc46e9ae7
angel-p57 <angel.prime57@gmail.com>
2026-01-04 13:32:53 +0900

    separate core functions to the library

---
+++ utils/lib/bg7utl.rb
@@ -0,0 +1,57 @@
+class BG7Manager
+  attr_reader :pids
+  def initialize(vmgr,imgmgr)
+    @vmgr=vmgr
+    @imgmgr=imgmgr
+    @arrowcolor=ImgManager.mkpixel(31,0,0)
+    @pids=[1,2,3,*10..26,34,253]
+    @prolog_pidmap={
+      1=>35,
+      2=>36,
+      3=>37,
+      14=>38,
+      16=>39,
+      19=>40,
+      20=>41,
+      21=>42,
+    }
+    @pallets={}
+  end
+  def haveanothertile(pid)
+    @prolog_pidmap.key?(pid)
+  end
+  def getpallet(pid)
+    pallet=@pallets[pid] and return pallet
+    pallet=@pallets[pid]=@vmgr.getbg7basecolors(pid).map{|r|
+      @imgmgr.mkpixel(*r)
+    }
+    return pallet if pid==253
+    pallet[7]=@arrowcolor if pid>3
+    if pid==24  # theater
+      @vmgr.getbg7rotcolors(pid,0,true).each_with_index{|r,i|
+        pallet[56+i]=@imgmgr.mkpixel(*r)
+      }
+    end
+    @vmgr.getbg7rotcolors(pid,0).each_with_index{|r,i|
+      pallet[113+i]=@imgmgr.mkpixel(*r)
+    }
+    return pallet
+  end
+  def getcoloredtiles(pid,prologue=false)
+    bgtiles=@vmgr.getmode7tiles(
+      pid,
+      pid==253 ? 0x3000 : 0x4000
+    )
+    if prologue && haveanothertile(pid)
+      bgtiles[192,64]=@vmgr.getmode7tiles(@prolog_pidmap[pid], 0x1000)
+    end
+    pallet=getpallet(pid)
+    bgtiles.map{|t|
+      t.map{|r|
+        r.map{|colid|
+          pallet[colid&0x7f]
+        }
+      }
+    }
+  end
+end
--- utils/mkbgimg.rb
+++ utils/mkbgimg.rb
@@ -1,42 +1,16 @@
 require_relative('lib/romutl')
 require_relative('lib/vramutl')
+require_relative('lib/bg7utl')
 require_relative('lib/img')
 
 romfile=ARGV[0]||SepRom::DEFAULT_ROM_PATH
 romobj=SepRom.new(romfile)
 vmgr=VRAMmgr.new(romobj)
+bg7mgr=BG7Manager.new(vmgr,ImgManager)
 
-arrowcolor=ImgManager.mkpixel(31,0,0)
-pids=[1,2,3,*10..26,34,253]
-prolog_pidmap={
-  1=>35,
-  2=>36,
-  3=>37,
-  14=>38,
-  16=>39,
-  19=>40,
-  20=>41,
-  21=>42,
-}
-pallets=pids.each_with_object({}){|pid,h|
-  h[pid]=pallet=vmgr.getbg7basecolors(pid).map{|r|
-    ImgManager.mkpixel(*r)
-  }
-  next if pid==253
-  pallet[7]=arrowcolor if pid>3
-  if pid==24  # theater
-    vmgr.getbg7rotcolors(pid,0,true).each_with_index{|r,i|
-      pallet[56+i]=ImgManager.mkpixel(*r)
-    }
-  end
-  vmgr.getbg7rotcolors(pid,0).each_with_index{|r,i|
-    pallet[113+i]=ImgManager.mkpixel(*r)
-  }
-}
-tiles2file=->tiles,pid,file,dotsize=3{
+tiles2file=->tiles,file,dotsize=3{
   warn "* invalid tile number #{tiles.size} *" if tiles.size%16!=0
   rown=tiles.size/16
-  pallet=pallets[pid]
   canvas=TiledCanvas.new(8,dotsize,rown,16)
   rown.times{|r|
     16.times{|c|
@@ -44,8 +18,7 @@ tiles2file=->tiles,pid,file,dotsize=3{
       t=tiles[r*16+c]
       8.times{|i|
         8.times{|j|
-          colid=t[i][j]&0x7f
-          win[i,j]=pallet[colid]
+          win[i,j]=t[i][j]
         }
       }
     }
@@ -53,17 +26,14 @@ tiles2file=->tiles,pid,file,dotsize=3{
   canvas.save(file)
 }
 
-pids.each{|pid|
-  bgtiles=vmgr.getmode7tiles(
-    pid,
-    pid==253 ? 0x3000 : 0x4000
-  )
+bg7mgr.pids.each{|pid|
   tiles2file[
-    bgtiles,
-    pid,
+    bg7mgr.getcoloredtiles(pid),
     pid==253 ? "bgbase-map.png" : "bgbase-%02d.png"%pid
   ]
-  prolog_pid=prolog_pidmap[pid] or next
-  bgtiles_ex=vmgr.getmode7tiles(prolog_pid, 0x1000)
-  tiles2file[bgtiles_ex,pid,"bgex-%02d.png"%pid]
+  bg7mgr.haveanothertile(pid) or next
+  tiles2file[
+    bg7mgr.getcoloredtiles(pid,true)[192,64],
+    "bgex-%02d.png"%pid
+  ]
 }
